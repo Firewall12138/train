07dfd0bdc9f0ef667a63b0ee4f7ec7a06d6c7a0a
Firewall12138 <fuhanqing@foxmail.com>
2024-10-31 22:14:10 +0800

    实现一个简单的注册接口

--- member/src/main/java/com/firewall/train/member/controller/MemberController.java
+++ member/src/main/java/com/firewall/train/member/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.firewall.train.member.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import com.firewall.train.member.domain.Member;
+import com.firewall.train.member.domain.MemberExample;
 import com.firewall.train.member.servece.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MemberController {
@@ -21,5 +27,10 @@ public class MemberController {
         int count = memberService.count();
         System.out.println(count);
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/firewall/train/member/servece/MemberService.java
+++ member/src/main/java/com/firewall/train/member/servece/MemberService.java
@@ -1,9 +1,14 @@
 package com.firewall.train.member.servece;
 
+import cn.hutool.core.collection.CollUtil;
+import com.firewall.train.member.domain.Member;
+import com.firewall.train.member.domain.MemberExample;
 import com.firewall.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +16,21 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        MemberExample example = new MemberExample();
+        example.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(example);
+        if(CollUtil.isNotEmpty(list)) {
+//            return list.get(0).getId();
+            throw new RuntimeException("该手机号已经注册过");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
